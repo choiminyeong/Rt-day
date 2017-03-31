77b6c714c1048edf39889143ad8ae0054929094f
choiminyoung <sedcreta12@gmail.com>
2017-03-31 18:23:14 +0900

    - 테스트겸, view파일 업로드

---
+++ application/views/user_v.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>정보 입력 페이지</title>
+	
+</head>
+<body>
+  유저 가입 view
+</body>
+</html>
