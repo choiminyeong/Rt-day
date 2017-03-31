93d5570e36fcb082c64f7dc6de1e9bbfe61f5adb
choiminyoung <sedcreta12@gmail.com>
2017-03-31 18:28:15 +0900

    - 입력 항목 내역추가

--- application/views/user_v.php
+++ application/views/user_v.php
@@ -5,9 +5,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>정보 입력 페이지</title>
-	
+
 </head>
 <body>
-  유저 가입 view
+  유저가 입력.
+    이름
+    지역
+    학교
+    나이
+    관심 분야
+    방문 계기
+    현재 섬기고 있는 교회 명
+    연락처
+    email
+
 </body>
 </html>
